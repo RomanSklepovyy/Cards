e040e6e08b9914919d0af44a711e076e43d0402d
Solomia <romansklepovyy1@gmail.com>
2019-11-04 10:17:44 +0200

    DeckComparator.compare

--- src/com/task3/carddeck/DeckComparator.java
+++ src/com/task3/carddeck/DeckComparator.java
@@ -1,19 +1,19 @@
 package com.task3.carddeck;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.lang.Integer;
 
 public class DeckComparator implements Comparator<Card> {
 
     public int compare(Card card1, Card card2) {
 
-        int value1 = o1.campus.compareTo(o2.campus);
+        int value1 = Integer.compare(Arrays.asList(Suit.values).indexOf(card1.getSuit()), Arrays.asList(Suit.values).indexOf(card2.getSuit()));
+
         if (value1 == 0) {
-            int value2 = o1.faculty.compareTo(o2.faculty);
-            if (value2 == 0) {
-                return o1.building.compareTo(o2.building);
-            } else {
-                return value2;
-            }
+            int value2 = Integer.compare(Arrays.asList(Rank.values).indexOf(card1.getRank()), Arrays.asList(Rank.values).indexOf(card2.getRank()));;
+            return value2;
         }
+
         return value1;
     }
 }
